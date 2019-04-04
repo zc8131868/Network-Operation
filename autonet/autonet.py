52501d9e0bf642b35aae4cdceada516b60a244cc
chengzheng <zc8131868@gmail.com>
2019-04-04 12:18:48 +0800

    add auto_check and close_connection methods

--- autonet/autonet.py
+++ autonet/autonet.py
@@ -1,9 +1,14 @@
 import telnetlib
 import subprocess
 import threading
+import pexpect
+import getpass
+import sys
+
 from IPy import IP
-
-
+child = ""
+show_result = ""
+path = ""
 class AutoNet(object):
 
 	def __init__(self, username, pwd, device_ios):
@@ -24,11 +29,6 @@ class AutoNet(object):
 
 #check reachibility of given single ip address
 	def PING_IP(self,ip):
-		# ping_able = [{"ping_able:":[]}]
-		# ping_unable = {"ping_unable:":[]}
-		# self._PING(ip, ping_able["ping_able:"], ping_unable["ping_unable:"])
-		# print("ping_able: %s" % ping_able["ping_able:"])
-		# print("ping_unable: %s" % ping_unable["ping_unable:"])
 		ping_able = []
 		ping_unable = []
 		ping_result = {"ping_able:":[], "ping_unable:":[]}
@@ -57,6 +57,71 @@ class AutoNet(object):
 		ping_result["ping_able:"] = ping_able
 		ping_result["ping_unable:"] = ping_unable
 		return(ping_result)
+
+# login device with telnet or ssh automatically
+	def auto_login(self, ip, username):
+		name = username
+		pwd = getpass.getpass("Pls input your password:")
+		global child
+		try:
+			telnetlib.Telnet(ip, 23, 5)
+			cmd_login_telnet = "telnet " + ip
+			child = pexpect.spawn(cmd_login_telnet, timeout=3, encoding='utf-8')
+			child.logfile_read = sys.stdout
+			index = child.expect(["username", "login", pexpect.TIMEOUT])
+			if index == 0 or index == 1:
+				child.sendline(name)
+				child.expect("assword")
+				try:
+					child.sendline(pwd)
+				except Exception as e:
+					print(e)
+			else:
+				print("Cannot telnet this ip address %s" % ip)
+
+		except:
+			cmd_login_ssh = "ssh -l " + name + " " + ip
+			child = pexpect.spawn(cmd_login_ssh, timeout=3,encoding='utf-8')
+			child.logfile_read = sys.stdout
+			index = child.expect(["assword", pexpect.TIMEOUT])
+			if index == 0:
+				try:
+					child.sendline(pwd)
+				except Exception as e:
+					print(e)
+
+# show information on device
+	def auto_show(self, cmd, URL=""):
+		global path
+		path = URL
+		if path == "":
+			child.expect("#")
+			child.sendline("ter len 0")
+			child.expect("#")
+			child.sendline(cmd)
+		else:
+			global show_result
+			show_result = open(path, "w")
+			child.expect("#")
+			child.sendline("ter len 0")
+			child.expect("#")
+			child.sendline(cmd)
+			child.logfile = show_result
+			#child.expect(pexpect.EOF)
+
+# Terminate the conncetion
+	def close_connection(self):
+		if path == "":
+			child.expect("#")
+			child.sendline("exit")
+			child.close()
+		else:
+			child.expect("#")
+			child.sendline("exit")			
+			child.close()
+			show_result.close()
+
+
 
 
 
