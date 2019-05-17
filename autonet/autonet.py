10203a068602e34bc6132fbdfbb2e615722566d2
chengzheng <zc8131868@gmail.com>
2019-05-17 17:23:30 +0800

    adopt threads for PING_IP

--- autonet/autonet.py
+++ autonet/autonet.py
@@ -33,22 +33,10 @@ class autonet(object):
 	def PING_IP(self,ip):
 		reachable = []
 		unreachable = []
-		ping_result = {"reachable:":[], "unreachable:":[]}
-		for i in ip:
-			self.__PING(i, reachable, unreachable)
-		ping_result["reachable:"] = reachable
-		ping_result["unreachable:"] = unreachable
-		return(ping_result)
-
-# check reachibility of ip addresses of given subnet
-	def PING_Subnet(self, subnet):
-		reachable = []
-		unreachable = []
-		ping_result = {"reachable:":[], "unreachable:":[]}
 		threads = []
-		subnet = IP(subnet)
-		for ip in subnet:
-			t = threading.Thread(target=self.__PING, args=(str(ip),reachable,unreachable,))
+		ping_result = {"reachable":[], "unreachable":[]}
+		for i in ip:
+			t = threading.Thread(target=self.__PING, args=(i,reachable,unreachable))
 			threads.append(t)
 
 		for i in range(len(threads)):
@@ -56,8 +44,29 @@ class autonet(object):
 
 		for i in range(len(threads)):
 			threads[i].join()
-		ping_result["reachable:"] = reachable
-		ping_result["unreachable:"] = unreachable
+
+		ping_result["reachable"] = reachable
+		ping_result["unreachable"] = unreachable
+		return(ping_result)
+
+# check reachibility of ip addresses of given subnet
+	def PING_Subnet(self, subnet):
+		reachable = []
+		unreachable = []
+		ping_result = {"reachable":[], "unreachable":[]}
+		threads = []
+		subnet = IP(subnet)
+		for ip in subnet:
+			t = threading.Thread(target=self.__PING, args=(str(ip),reachable,unreachable))
+			threads.append(t)
+
+		for i in range(len(threads)):
+			threads[i].start()
+
+		for i in range(len(threads)):
+			threads[i].join()
+		ping_result["reachable"] = reachable
+		ping_result["unreachable"] = unreachable
 		return(ping_result)
 
 # show command via telnet or ssh
@@ -118,7 +127,7 @@ class autonet(object):
 			else:
 				print("Cannot ssh this ip address %s" % ip)
 		except:
-			print("Pls check the %s or command and try again :(" % ip)
+			print("Pls check %s or command and try again" % ip)
 			self.error_file.write(ip)
 			self.error_file.write("\n")
 			self.error_file.write("#########################")
@@ -200,7 +209,7 @@ class autonet(object):
 			else:
 				print("Cannot ssh this ip address %s" % ip)
 		except:
-			print("Pls check the %s or command and try again:(" % ip)
+			print("Pls check %s or command and try again" % ip)
 			self.error_file.write(ip)
 			self.error_file.write("\n")
 			self.error_file.write("#########################")
@@ -286,245 +295,222 @@ class autonet(object):
 			else:
 				print("Cannot ssh this ip address %s" % ip)
 		except:
-			print("Pls check the %s or command and try again:(" % ip)
+			print("Pls check %s or command and try again" % ip)
 			self.error_file.write(ip)
 			self.error_file.write("\n")
 			self.error_file.write("#########################")
 
 # close log
-	def close_logging(self):
-		try:		
-			self.__child.close()
-			self.result_file.close()
-		except AttributeError:
-			self.error_file.close()
-		except:		
-			self.__child.close()
-			self.result_file.close()
-			self.error_file.close()	
-			
+	def close_logging(self):	
+		self.__child.close()
+		self.result_file.close()
+		self.error_file.close()	
 	
 # Get chassis serial number
 	def get_serial_number(self, ip):
-		original_file = open("sn_log.txt", "w+")
-		try:
-			telnetlib.Telnet(ip, 23, 5)
-			cmd_login_telnet = "telnet " + ip	
-			self.__child = pexpect.spawn(cmd_login_telnet, timeout=20, encoding='utf-8')
-			self.__child.logfile_read = original_file
-			index = self.__child.expect(["username", "login", pexpect.TIMEOUT])
-			if index == 0 or index == 1:
-				self.__child.sendline(self.username)
-				self.__child.expect("assword")
-				try:
-					self.__child.sendline(self.password)
-					self.__child.expect("#")
-					if self.device_ios == "cisco_nxos":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show sprom backplane 1")
-					elif self.device_ios == "cisco_ios":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "rg_os":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					self.__child.expect("#")
-					self.__child.sendline("exit")
-				except Exception as e:
-					print("check username or password again")
-			else:
-				print("Cannot telnet this ip address %s" % ip)
-
-			original_file.flush()
-			original_file.seek(0)
-			if self.device_ios == "cisco_nxos":
-				sn = methodlib.cisco_nxos_sn(original_file)
-			elif self.device_ios == "cisco_ios":
-				sn = methodlib.cisco_ios_sn(original_file)
-			elif self.device_ios == "rg_os":
-				sn = methodlib.rg_os_sn(original_file)
-			# print(sn)
-			return(sn)
-
-		except ConnectionRefusedError:
-			cmd_login_ssh = "ssh -l " + self.username+ " " + ip
-			self.__child = pexpect.spawn(cmd_login_ssh, timeout=20, encoding='utf-8')
-			self.__child.logfile_read = original_file
-			index = self.__child.expect(["assword", "yes/no", pexpect.TIMEOUT])
-			if index == 0:
-				try:
-					self.__child.sendline(self.password)
-					self.__child.expect("#")
-					if self.device_ios == "cisco_nxos":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show sprom backplane 1")
-					elif self.device_ios == "cisco_ios":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "rg_os":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					self.__child.expect("#")
-					self.__child.sendline("exit")
-				except Exception as e:
-					print(e)
-			elif index == 1:
-				try:
-					self.__child.sendline("yes")
+		with open("sn_log.txt", "w+") as original_file:
+			try:
+				telnetlib.Telnet(ip, 23, 5)
+				cmd_login_telnet = "telnet " + ip
+				self.__child = pexpect.spawn(cmd_login_telnet, timeout=20, encoding="utf-8")
+				self.__child.logfile_read = original_file
+				# self.__child.logfile = sys.stdout
+				index = self.__child.expect(["username", "login", pexpect.TIMEOUT])
+				if index == 0 or index == 1:
+					self.__child.sendline(self.username)
 					self.__child.expect("assword")
-					self.__child.sendline(self.password)
-					self.__child.expect("#")
-					if self.device_ios == "cisco_nxos":
-						self.__child.sendline("ter len 0")
+					try:
+						self.__child.sendline(self.password)
 						self.__child.expect("#")
-						self.__child.sendline("show sprom backplane 1")
-					elif self.device_ios == "cisco_ios":
-						self.__child.sendline("ter len 0")
+						if self.device_ios == "cisco_nxos":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show sprom backplane 1")
+						elif self.device_ios == "cisco_ios":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						elif self.device_ios == "rg_os":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show ver")
 						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "rg_os":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					self.__child.expect("#")
-					self.__child.sendline("exit")	
-				except Exception as e:
-					print(e)				
-			else:
-				print("Cannot ssh this ip address %s" % ip)		
+						self.sendline("exit")
+					except Exception as e:
+						print("check username or password again")
+				else:
+					print("Cannot tlenet this ip address %s" % ip)
 
-			original_file.flush()
-			original_file.seek(0)
-			if self.device_ios == "cisco_nxos":
-				sn = methodlib.cisco_nxos_sn(original_file)
-			elif self.device_ios == "cisco_ios":
-				sn = methodlib.cisco_ios_sn(original_file)
-			elif self.device_ios == "rg_os":
-				sn = methodlib.rg_os_sn(original_file)
-			# print(sn)
-			return(sn)
-		except:
-			print("Pls check the %s or command and try again :(" % ip)
-			self.error_file.write(ip)
-			self.error_file.write("\n")
-			self.error_file.write("#########################")
+				original_file.seek(0)
+
+				if self.device_ios == "cisco_nxos":
+					sn = methodlib.cisco_nxos_sn(original_file)
+				elif self.device_ios == "cisco_ios":
+					sn = methodlib.cisco_ios_sn(original_file)
+				elif self.device_ios == "rg_os":
+					sn = methodlib.rg_os_sn(original_file)
+				return(sn)
+
+			except ConnectionRefusedError:
+				cmd_login_ssh = "ssh -l "+ self.username + " " + ip
+				self.__child = pexpect.spawn(cmd_login_ssh, timeout=20, encoding="utf-8")
+				self.__child.logfile_read = original_file
+				# self.__child.logfile = sys.stdout
+				index = self.__child.expect(["assword", "yes/no", pexpect.TIMEOUT])
+				if index == 0:
+					try:
+						self.__child.sendline(self.password)
+						self.__child.expect("#")
+						if self.device_ios == "cisco_nxos":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show sprom backplane 1")
+						elif self.device_ios == "cisco_ios":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						elif self.device_ios == "rg_os":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						self.__child.expect("#")
+						self.__child.sendline("exit")
+					except Exception as e:
+						print(e)
+				elif index == 1:
+					try:
+						self.__child.sendline("yes")
+						self.__child.expect("assword")
+						self.__child.sendline(self.password)
+						self.__child.expect("#")
+						if self.device_ios == "cisco_nxos":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show sprom backplane 1")
+						elif self.device_ios == "cisco_ios":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						elif self.device_ios == "rg_os":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						self.__child.expect("#")
+						self.__child.sendline("exit")
+					except Exception as e:
+						print(e)
+				else:
+					print("Cannot ssh this ip address %s" % ip)
+
+				original_file.seek(0)
+
+				if self.device_ios == "cisco_nxos":
+					sn = methodlib.cisco_nxos_sn(original_file)
+				elif self.device_ios == "cisco_ios":
+					sn = methodlib.cisco_ios_sn(original_file)
+				elif self.device_ios == "rg_os":
+					sn = methodlib.rg_os_sn(original_file)
+				return(sn)
+
+			except:
+				print("Pls check %s or command and try again" % ip)
+				self.error_file.write(ip)
+				self.error_file.write("\n")
+				self.error_file.write("#########################")				
 
 
 
 # Get software version
 	def get_version(self, ip):
-		original_file = open("soft_version.txt", "w+")
-		try:
-			telnetlib.Telnet(ip, 23, 5)
-			cmd_login_telnet = "telnet " + ip	
-			self.__child = pexpect.spawn(cmd_login_telnet, timeout=20, encoding='utf-8')
-			self.__child.logfile_read = original_file
-			index = self.__child.expect(["username", "login", pexpect.TIMEOUT])
-			if index == 0 or index == 1:
-				self.__child.sendline(self.username)
-				self.__child.expect("assword")
-				try:
-					self.__child.sendline(self.password)
-					self.__child.expect("#")
-					if self.device_ios == "cisco_nxos":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "cisco_ios":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "rg_os":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					self.__child.expect("#")
-					self.__child.sendline("exit")
-				except Exception as e:
-					print("check username or password again")
-			else:
-				print("Cannot telnet this ip address %s" % ip)
-
-			original_file.flush()
-			original_file.seek(0)
-			if self.device_ios == "cisco_nxos":
-				sv = methodlib.cisco_nxos_sv(original_file)
-			elif self.device_ios == "cisco_ios":
-				sv = methodlib.cisco_ios_sv(original_file)
-			elif self.device_ios == "rg_os":
-				sv = methodlib.rg_os_sv(original_file)
-			return(sv)
-
-		except ConnectionRefusedError:
-			cmd_login_ssh = "ssh -l " + self.username+ " " + ip
-			self.__child = pexpect.spawn(cmd_login_ssh, timeout=20, encoding='utf-8')
-			self.__child.logfile_read = original_file
-			index = self.__child.expect(["assword", "yes/no", pexpect.TIMEOUT])
-			if index == 0:
-				try:
-					self.__child.sendline(self.password)
-					self.__child.expect("#")
-					if self.device_ios == "cisco_nxos":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "cisco_ios":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "rg_os":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					self.__child.expect("#")
-					self.__child.sendline("exit")
-				except Exception as e:
-					print(e)
-			elif index == 1:
-				try:
-					self.__child.sendline("yes")
+		with open("soft_version.txt", "w+") as original_file:
+			try:
+				telnetlib.Telnet(ip, 23, 5)
+				cmd_login_telnet = "telnet " + ip
+				self.__child = pexpect.spawn(cmd_login_telnet, timeout=20, encoding="utf-8")
+				self.__child.logfile_read = original_file
+				index = self.__child.expect(["username", "login", pexpect.TIMEOUT])
+				if index == 0 or index == 1:
+					self.__child.sendline(self.username)
 					self.__child.expect("assword")
-					self.__child.sendline(self.password)
-					self.__child.expect("#")
-					if self.device_ios == "cisco_nxos":
-						self.__child.sendline("ter len 0")
+					try:
+						self.__child.sendline(self.password)
 						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "cisco_ios":
-						self.__child.sendline("ter len 0")
+						if self.device_ios == "cisco_nxos" or self.device_ios == "cisco_ios" or self.device_ios == "rg_os":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						else:
+							print("Pls check device_ios")
 						self.__child.expect("#")
-						self.__child.sendline("show version")
-					elif self.device_ios == "rg_os":
-						self.__child.sendline("ter len 0")
-						self.__child.expect("#")
-						self.__child.sendline("show version")
-					self.__child.expect("#")
-					self.__child.sendline("exit")	
-				except Exception as e:
-					print(e)				
-			else:
-				print("Cannot ssh this ip address %s" % ip)		
+						self.__child.sendline("exit")
+					except Exception as e:
+						print("check username or password again")
+				else:
+					print("Cannot telnet this ip address %s" % ip)
 
-			original_file.flush()
-			original_file.seek(0)
-			if self.device_ios == "cisco_nxos":
-				sv = methodlib.cisco_nxos_sv(original_file)
-			elif self.device_ios == "cisco_ios":
-				sv = methodlib.cisco_ios_sv(original_file)
-			elif self.device_ios == "rg_os":
-				sv = methodlib.rg_os_sv(original_file)
-			return(sv)
-		except:
-			print("Pls check the %s or command and try again :(" % ip)
-			self.error_file.write(ip)
-			self.error_file.write("\n")
-			self.error_file.write("#########################")
+				original_file.seek(0)
+
+				if self.device_ios == "cisco_nxos":
+					sv = methodlib.cisco_nxos_sv(original_file)
+				elif self.device_ios == "cisco_ios":
+					sv = methodlib.cisco_ios_sv(original_file)
+				elif self.device_ios == "rg_os":
+					sv = methodlib.rg_os_sv(original_file)
+				return(sv)
+
+			except ConnectionRefusedError:
+				cmd_login_ssh = "ssh -l " + self.username+ " " + ip
+				self.__child = pexpect.spawn(cmd_login_ssh, timeout=20, encoding='utf-8')
+				self.__child.logfile_read = original_file
+				index = self.__child.expect(["assword", "yes/no", pexpect.TIMEOUT])
+				if index == 0:
+					try:
+						self.__child.sendline(self.password)
+						self.__child.expect("#")
+						if self.device_ios == "cisco_nxos" or self.device_ios == "cisco_ios" or self.device_ios == "rg_os":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						else:
+							print("Pls check device_ios")
+						self.__child.expect("#")
+						self.__child.sendline("exit")
+					except Exception as e:
+						print(e)
+				elif index == 1:
+					try:
+						self.__child.sendline("yes")
+						self.__child.expect("assword")
+						self.__child.sendline(self.password)
+						self.__child.expect("#")
+						if self.device_ios == "cisco_nxos" or self.device_ios == "cisco_ios" or self.device_ios == "rg_os":
+							self.__child.sendline("ter len 0")
+							self.__child.expect("#")
+							self.__child.sendline("show version")
+						else:
+							print("Pls check device_ios")
+						self.__child.expect("#")
+						self.__child.sendline("exit")
+					except Exception as e:
+						print(e)						
+				else:
+					print("Cannot ssh this ip address %s" % ip)	
+
+				original_file.seek(0)
+
+				if self.device_ios == "cisco_nxos":
+					sv = methodlib.cisco_nxos_sv(original_file)
+				elif self.device_ios == "cisco_ios":
+					sv = methodlib.cisco_ios_sv(original_file)
+				elif self.device_ios == "rg_os":
+					sv = methodlib.rg_os_sv(original_file)
+				return(sv)
+
+			except:
+				print("Pls check %s or command and try again" % ip)
+				self.error_file.write(ip)
+				self.error_file.write("\n")
+				self.error_file.write("#########################")	
 
 
 		
